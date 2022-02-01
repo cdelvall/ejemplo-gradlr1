ed88f58564d6b5e96ebb48d9fe3a08c043ba2f28
Constanza Del Valle <constanza.delvalle.s@gmail.com>
2022-02-01 15:12:48 -0300

    modificacion de variables

--- gradle.groovy
+++ gradle.groovy
@@ -10,7 +10,7 @@ def call(){
     }
     stage("Paso 3: Curl Springboot Gradle sleep 20"){
         sh "gradle bootRun&"
-        sh "sleep 20 && curl -X GET 'http://localhost:10001/rest/mscovid/test?msg=testing'"
+        sh "sleep 20 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
     }
     stage("Paso 4: Subir Nexus"){
         nexusPublisher nexusInstanceId: 'nexus',
@@ -33,13 +33,13 @@ def call(){
         ]
     }
     stage("Paso 5: Descargar Nexus"){
-        sh ' curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD "http://nexus:10001/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar" -O'
+        sh ' curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD "http://nexus:8081/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar" -O'
     }
     stage("Paso 6: Levantar Artefacto Jar"){
         sh 'nohup bash java -jar DevOpsUsach2020-0.0.1.jar & >/dev/null'
     }
     stage("Paso 7: Testear Artefacto - Dormir(Esperar 20sg) "){
-        sh "sleep 20 && curl -X GET 'http://localhost:10001/rest/mscovid/test?msg=testing'"
+        sh "sleep 20 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
     }
 }
 return this;
--- maven.groovy
+++ maven.groovy
@@ -16,7 +16,7 @@ def call(){
   }
   stage("Paso 5: Curl Springboot Gradle sleep 20"){
       sh "gradle bootRun&"
-      sh "sleep 20 && curl -X GET 'http://localhost:10001/rest/mscovid/test?msg=testing'"
+      sh "sleep 20 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
   }
   stage("Paso 6: Subir Nexus"){
       nexusPublisher nexusInstanceId: 'nexus',
@@ -39,13 +39,13 @@ def call(){
       ]
   }
   stage("Paso 7: Descargar Nexus"){
-      sh ' curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD "http://nexus:10001/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar" -O'
+      sh ' curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD "http://nexus:8081/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar" -O'
   }
   stage("Paso 8: Levantar Artefacto Jar"){
       sh 'nohup bash java -jar DevOpsUsach2020-0.0.1.jar & >/dev/null'
   }
   stage("Paso 9: Testear Artefacto - Dormir(Esperar 20sg) "){
-      sh "sleep 20 && curl -X GET 'http://localhost:10001/rest/mscovid/test?msg=testing'"
+      sh "sleep 20 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
   }
 }
 return this;
