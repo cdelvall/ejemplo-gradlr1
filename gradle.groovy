90bb995c706d1e1d80cc1a1b9734e22a1bb677e8
Constanza Del Valle <constanza.delvalle.s@gmail.com>
2022-02-03 19:48:02 -0300

    modificar nexus a nexus3

--- gradle.groovy
+++ gradle.groovy
@@ -13,7 +13,7 @@ def call(){
         sh "sleep 20 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
     }
     stage("Paso 4: Subir Nexus"){
-        nexusPublisher nexusInstanceId: 'nexus',
+        nexusPublisher nexusInstanceId: 'nexus3',
         nexusRepositoryId: 'devops-usach-nexus',
         packages: [
             [$class: 'MavenPackage',
@@ -33,7 +33,7 @@ def call(){
         ]
     }
     stage("Paso 5: Descargar Nexus"){
-        sh ' curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD "http://nexus:8080/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar" -O'
+        sh ' curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD "http://nexus3:8080/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar" -O'
     }
     stage("Paso 6: Levantar Artefacto Jar"){
         sh 'nohup bash java -jar DevOpsUsach2020-0.0.1.jar & >/dev/null'
--- maven.groovy
+++ maven.groovy
@@ -19,7 +19,7 @@ def call(){
       sh "sleep 20 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
   }
   stage("Paso 6: Subir Nexus"){
-      nexusPublisher nexusInstanceId: 'nexus',
+      nexusPublisher nexusInstanceId: 'nexus3',
       nexusRepositoryId: 'devops-usach-nexus',
       packages: [
           [$class: 'MavenPackage',
@@ -39,7 +39,7 @@ def call(){
       ]
   }
   stage("Paso 7: Descargar Nexus"){
-      sh ' curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD "http://nexus:8081/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar" -O'
+      sh ' curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD "http://nexus3:8081/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar" -O'
   }
   stage("Paso 8: Levantar Artefacto Jar"){
       sh 'nohup bash java -jar DevOpsUsach2020-0.0.1.jar & >/dev/null'
